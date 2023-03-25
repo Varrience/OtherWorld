a36e2493041c4ba449296b53259c3ceea5ecb4e1
LGM-Productions <96096550+LGM-Productions@users.noreply.github.com>
2023-03-25 15:23:11 -0400

    Create global.ts

---
+++ Frontend/global.ts
@@ -0,0 +1,29 @@
+//Global Functions
+var client = {
+    info: {
+        CDO_ID: getUserId(),
+    },
+    session: {
+        startStamp: window.getTime(),
+        dateStart: Date().toString(),
+    },
+    currentScreen: "__index",
+    ranks: {
+        admin: "🔨",
+        mod: "⚒️",
+        dev: "🛠️",
+    },
+};
+
+function changeScreen(newScreen) {
+  var oldScreen = client.currentScreen;
+  setStyle(oldScreen, "transform: translateX(-320px) rotate(-20deg); opacity: 0; transition: 0.45s;");
+  setTimeout(function() {
+    setStyle(newScreen, "transform: translateX(320px) rotate(20deg); opacity: 0; transition: 0s;");
+    setScreen(newScreen);
+    setTimeout(function(){
+      client.currentScreen = newScreen;
+      setStyle(newScreen, "transform: translateX(0px) rotate(0deg); opacity: 1; transition: 0.45s;");
+    }, 150);
+  }, 450);
+}
